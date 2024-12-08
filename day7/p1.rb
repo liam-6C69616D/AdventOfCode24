788781f56e9316384369ec0b410370eeebc2209a
Liam Smith <liamjsmith02@gmail.com>
2024-12-08 21:58:46 +0000

    Day 7 Part 2

---
+++ day7/p1.rb
@@ -0,0 +1,45 @@
+targets = []
+parts = []
+
+File.foreach('input.txt') do |line|
+  lineSplit = line.split(':')
+  targets.push(lineSplit[0].to_i) 
+  numbers = lineSplit[1].split.map { |elem| elem.to_i }
+  parts.push(numbers)
+end
+
+def permute(numbers, target)
+  found = false
+  operations = ['+', '*'].repeated_permutation(numbers.length - 1).to_a
+  operations.each do |op|
+    if evaluate(numbers, op) == target
+      found = true
+      break
+    end
+  end
+
+  return found
+end
+
+def evaluate(numbers, operations)
+  total = numbers[0]
+  for i in 0...operations.length
+    if operations[i] == '+'
+      total += numbers[i+1]
+    else
+      total *= numbers[i+1]
+    end
+  end
+
+  return total
+end
+
+sum = 0
+
+for i in 0...targets.length
+  if permute(parts[i], targets[i])
+    sum += targets[i]
+  end
+end
+
+puts sum
